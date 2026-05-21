57faec1f93bde8b63d0220488e3ef6d60479a35f
eshybishy <k_kanesha@yahoo.com>
2026-05-21 10:16:42 +1000

    Update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+python .github/scripts/frequency.py
+bash .github/scripts/update_readme.sh
+
+git config --global user.name "github-actions[bot]"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+
+git add README.md vowel_results.txt
+git commit -m "Update README with vowel frequency results" || echo "No changes to commit"
+git push
