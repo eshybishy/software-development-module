554f0d8593c8bf5d1fc8f4f416052b0f4ec10d5a
eshybishy <k_kanesha@yahoo.com>
2026-05-21 10:20:00 +1000

    Update update_readme.sh

--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+FREQ_RESULT=$1
+GITHUB_USER=$2
+TIMESTAMP=$(date)
+
+echo -e "\n${GITHUB_USER} - ${FREQ_RESULT} - ${TIMESTAMP}" >> README.md
+
+git config --global user.name "github-actions"
+git config --global user.email "github-actions@users.noreply.github.com"
+
+git add README.md
+git commit -m "Update README with vowel frequency results" || echo "No changes to commit"
+git push
