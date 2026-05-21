0fef85b4974c771cc32c3595a1eb31cde3e0a253
eshybishy <k_kanesha@yahoo.com>
2026-05-21 10:20:38 +1000

    Update entrypoint.sh

--- .github/scripts/entrypoint.sh
+++ .github/scripts/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-python .github/scripts/frequency.py
-bash .github/scripts/update_readme.sh
+echo "Starting Frequency Analyzer..."
 
-git config --global user.name "github-actions[bot]"
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
+FREQ_RESULT=$(python3 /app/.github/scripts/frequency.py /app/data.txt)
 
-git add README.md vowel_results.txt
-git commit -m "Update README with vowel frequency results" || echo "No changes to commit"
-git push
+bash /app/.github/scripts/update_readme.sh "$FREQ_RESULT" "$GITHUB_USER"
+
+echo "Process Completed!"
